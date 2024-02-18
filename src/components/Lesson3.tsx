fd26b64071a9440d4e257e59047882f87479561b
kuni-chan-k <kentaro@kuni-chan.com>
2024-02-18 19:22:33 +0900

    DnD機能実装

--- src/components/Lesson3.tsx
+++ src/components/Lesson3.tsx
@@ -1,4 +1,8 @@
 import { useState, useCallback, useEffect } from 'react';
+import { DndContext, closestCenter, KeyboardSensor, PointerSensor, useSensor, useSensors, DragEndEvent, TouchSensor, Modifiers } from '@dnd-kit/core';
+import { SortableContext, sortableKeyboardCoordinates, verticalListSortingStrategy, useSortable } from '@dnd-kit/sortable';
+import { arrayMove } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
 
 // レッスン3 Todo
 // https://qiita.com/kmmch/items/107bfeb1d316e4702244
@@ -21,7 +25,6 @@ function Lesson3() {
   const [isViewTodoAddModal, setIsViewTodoAddModal] = useState(false);
   const [inputNewTask, setInputNewTask] = useState("");
   const [hasInputError, setHasInputError] = useState(false);
-  const [dragOverId, setDragOverId] = useState<number | null>(null);
 
   // タスク名の入力最大値
   const MAXLENGTH = 1000;
@@ -78,49 +81,68 @@ function Lesson3() {
   };
 
   // ドラッグ&ドロップ
-  const handleDragStart = useCallback((e: React.DragEvent<HTMLDivElement>, id: number) => {
-    e.dataTransfer.setData("text/plain", id.toString());
-  }, []);
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(TouchSensor),
+    useSensor(KeyboardSensor, { coordinateGetter: sortableKeyboardCoordinates }),
+  );
+  const handleDragStart = () => {
+    // ドラッグ開始時にページ全体のスクロールを無効化
+    document.body.style.overflow = 'hidden';
+    document.body.style.touchAction = 'none'; // スマホ表示でのスクロールを防ぐ
+    document.addEventListener('touchmove', preventTouchMove, { passive: false }); // タッチによるスクロールを防ぐイベントリスナーを追加
+  };
+  const handleDragEnd = (event: DragEndEvent) => {
+    // ドラッグ終了時にスクロールを再有効化
+    document.body.style.overflow = '';
+    document.body.style.touchAction = ''; // スクロールを再び許可
+    document.removeEventListener('touchmove', preventTouchMove); // イベントリスナーを削除
+    const { active, over } = event;
+    if (over && active.id !== over.id) {
+      const oldIndex = tasks.findIndex((task) => task.id === active.id);
+      const newIndex = tasks.findIndex((task) => task.id === over.id);
+      setTasks((tasks) => arrayMove(tasks, oldIndex, newIndex));
+    }
+  };
+  const preventTouchMove: (event: TouchEvent) => void = (event) => {
+    // タッチによるスクロールを防ぐための関数
+    event.preventDefault();
+  };
+  const restrictToVerticalAxis: Modifiers = [({ transform }) => ({
+    // 垂直方向のみへのドラッグを制限
+      ...transform,
+      x: 0
+    }),
+  ];
 
-  const handleDrop = useCallback((e: React.DragEvent<HTMLDivElement>, dropTaskId: number) => {
-    e.preventDefault(); // デフォルトのドロップ処理をキャンセル
 
-    const draggedId = e.dataTransfer.getData("text/plain");
-    const draggedTaskId = Number(draggedId);
-
-    if (!draggedId) return;
-
-    const draggedTaskIndex = tasks.findIndex(task => task.id === draggedTaskId);
-    const dropTaskIndex = tasks.findIndex(task => task.id === dropTaskId);
-
-    if (draggedTaskIndex === -1 || dropTaskIndex === -1) return; 
-
-    const newTasks = [...tasks];
-    const [removedTask] = newTasks.splice(draggedTaskIndex, 1);
-    newTasks.splice(dropTaskIndex, 0, removedTask);
-
-    setTasks(newTasks);
-  }, [tasks]);
-
+  // タスク(1行表示)
   const TaskItem: React.FC<TaskItemProps> = ({ task, onToggle, onDelete }) => {
+    const {
+      attributes,
+      listeners,
+      setNodeRef,
+      transform,
+      transition
+    } = useSortable({ id: task.id });
+    const style = {
+      transform: CSS.Transform.toString(transform),
+      transition,
+    };
+
     return (
       <div
-        className={`mx-auto flex w-full max-w-screen-md items-center justify-between rounded-2xl bg-white p-8 drop-shadow-md
-          ${dragOverId === task.id ? 'border-2 border-gray-200' : ''}
-        `}
-        draggable="true"
-        onDragStart={(e) => handleDragStart(e, task.id)}
-        onDragOver={(e) => {
-          e.preventDefault();
-          setDragOverId(task.id);
-        }}
-        onDragLeave={() => setDragOverId(null)}
-        onDrop={(e) => {
-          handleDrop(e, task.id);
-          setDragOverId(null); // ドロップ後にドラッグオーバー状態をリセット
-        }}
+        ref={setNodeRef}
+        style={style}
+        {...attributes}
+        className="mx-auto flex w-full max-w-screen-md cursor-default items-center justify-between rounded-2xl bg-white p-6 drop-shadow-md md:p-8"
       >
         <div className="flex items-center text-xl">
+          <button {...listeners} className="mr-2 cursor-pointer p-2">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="size-8">
+              <path d="M7,19V17H9V19H7M11,19V17H13V19H11M15,19V17H17V19H15M7,15V13H9V15H7M11,15V13H13V15H11M15,15V13H17V15H15M7,11V9H9V11H7M11,11V9H13V11H11M15,11V9H17V11H15M7,7V5H9V7H7M11,7V5H13V7H11M15,7V5H17V7H15Z" />
+            </svg>
+          </button>
           <input
             type="checkbox"
             id={`task-${task.id}`}
@@ -132,9 +154,7 @@ function Lesson3() {
             {task.name}
           </span>
         </div>
-        <button
-          onClick={() => onDelete(task.id)}
-        >
+        <button onClick={() => onDelete(task.id)}>
           <svg className="size-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
           </svg>
@@ -182,16 +202,26 @@ function Lesson3() {
           <p className="text-center text-xl font-bold">
             Todoは登録されていません
           </p>
-        ) : 
-          tasks.map((task) => (
-            <TaskItem
-              key={task.id}
-              task={task}
-              onToggle={toggleTask}
-              onDelete={deleteTask}
-            />
-          ))
-        }
+        ) : (
+          <DndContext
+            sensors={sensors}
+            collisionDetection={closestCenter}
+            onDragStart={handleDragStart}
+            onDragEnd={handleDragEnd}
+            modifiers={restrictToVerticalAxis}
+          >
+            <SortableContext items={tasks} strategy={verticalListSortingStrategy}>
+              {tasks.map((task) => (
+                <TaskItem
+                  key={task.id}
+                  task={task}
+                  onToggle={toggleTask}
+                  onDelete={deleteTask}
+                />
+              ))}
+            </SortableContext>
+          </DndContext>
+        )}
 
         <div className='flex justify-center gap-6'>
           <button
