f72db1d80a00b8fd5aa3a8d730b2dea2b814df90
kuni-chan-k <kentaro@kuni-chan.com>
2024-02-18 16:03:28 +0900

    各ページでリロードすると404になる問題

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Error404 from './pages/404';
 import TopPage from './pages/TopPage';
 import LessonPage1 from './pages/LessonPage1';
@@ -7,15 +7,15 @@ import LessonPage3 from './pages/LessonPage3';
 
 function App() {
   return (
-    <BrowserRouter basename="/react-lesson/">
+    <HashRouter basename="/">
       <Routes>
         <Route path="/" element={<TopPage />} />
-        <Route path="lesson1" element={<LessonPage1 />} />
-        <Route path="lesson2" element={<LessonPage2 />} />
-        <Route path="lesson3" element={<LessonPage3 />} />
+        <Route path="/lesson1" element={<LessonPage1 />} />
+        <Route path="/lesson2" element={<LessonPage2 />} />
+        <Route path="/lesson3" element={<LessonPage3 />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
